1e7869c5185ac8ebed45442074a10ec4f8a798f4
samuelPC <2876763976@qq.com>
2021-05-19 13:56:25 +0800

    面向对象实战之yaml数据

--- Script_exercise/python_code/Data/demo.py
+++ Script_exercise/python_code/Data/demo.py
@@ -1,5 +1,10 @@
 """"
 pyyaml的使用示例
+用于数据驱动存储数据
+YAML 支持以下几种数据类型：
+对象：键值对的集合，又称为映射（mapping）/ 哈希（hashes） / 字典（dictionary）
+数组：一组按次序排列的值，又称为序列（sequence） / 列表（list）
+纯量（scalars）：单个的、不可再分的值
 """
 import sys
 
