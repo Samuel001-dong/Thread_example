a7e50637fc1074df0616314a0d91b0217d14dbe8
samuelPC <2876763976@qq.com>
2021-05-27 17:30:05 +0800

    pytest测试实战

--- unittest_example/pytest_dir/test_calc.py
+++ unittest_example/pytest_dir/test_calc.py
@@ -2,15 +2,18 @@
 
 
 """
-# 被测代码
 import pytest
+# 导入被测代码
+import yaml
+
+from calculator import CalculatorExercise
 
 
 # 测试代码
-from pytest_dir.calculator import CalculatorExercise
-
-
 class TestCalc:
+    with open("./calc_data.yml") as f:
+        DATA = yaml.safe_load(f)
+
     def setup_class(self):
         print("类级别：setup_class")
         calc = CalculatorExercise()
@@ -22,17 +25,22 @@ class TestCalc:
         self.calc = CalculatorExercise()
         print("方法级别：“setup”")
 
-    @pytest.mark.parametrize("a, b, except1", [(1, 2, 3), (4, 5, 9)])
+    @pytest.mark.parametrize("a, b, except1", DATA["add_int"]["data"])
     def test_add_int(self, a, b, except1):
         assert self.calc.calc_add(a, b) == except1
 
-    @pytest.mark.parametrize("a, b, except1", [(0.1, 0.2, 0.3), (0.4, 0.5, 0.9)])
+    @pytest.mark.parametrize("a, b, except1", DATA["add_float"]["data"], ids=DATA["add_float"]["ids"])   # 数据驱动示例
     def test_add_float(self, a, b, except1):
         assert round(self.calc.calc_add(a, b), 3) == except1
 
     @pytest.mark.parametrize("a, b, except1", [(1, 2, 0.5), (40, 5, 8)])
     def test_div(self, a, b, except1):
         assert self.calc.calc_div(a, b) == except1
+
+    @pytest.mark.parametrize("a, b, except1", [(1, 0, 0.5)])
+    def test_div_zero(self, a, b, except1):
+        with pytest.raises(ZeroDivisionError):      # 抛出异常，认为此异常为正常现象
+            assert self.calc.calc_div(a, b) == except1
 
     @pytest.mark.parametrize("a, b, except1", [(1, 2, -1), (40, 5, 35)])
     def test_sub(self, a, b, except1):
